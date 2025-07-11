705da4cef436110c04294c3eba4bf8fef03591e5
prachisehrawatt <sk6472692@gmail.com>
2025-07-11 08:03:23 +0000

    csv-codes

--- cs_12/8.py
+++ cs_12/8.py
@@ -1,27 +1,11 @@
-'''1. Student Marks Manager
-Create a CSV file named `students.csv` with the following columns:
-`roll_no`, `name`, `math`, `science`, `english`
-Now, write a Python program to:
-- Read the CSV file.
-- Calculate the total and average marks for each student.
-- Print the result as a formatted table.'''
-
 import csv
-f=open('extension.csv','r')
-obj= csv.reader(f)
-students = []
-
-for row in obj:
-        math = row['math']
-        science = row['science']
-        english = row['english']
-
-        total = math+science+english
-        average = round(total / 3)
-
-        row['total'] = total
-        row['average'] = average
-
-        students.append(row)
-
-
+f=open("stu.csv","r")
+obj=csv.reader(f, delimiter='\t')
+stu_count = 0
+for i in obj:
+    if stu_count == 0:
+        print(",".join(i) , "average marks")
+        stu_count = stu_count + 1
+    else:
+        avg_marks = (int(i[2]) + int(i[3]) + int(i[4])) / 3
+        print(' '.join(i),' \t ',int(avg_marks))
